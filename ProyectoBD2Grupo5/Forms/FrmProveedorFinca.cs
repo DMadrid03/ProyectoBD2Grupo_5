99831b0ef28a772a33f552fc44ab93d77fad6b52
Jefferson castro <jeffersoncastro947@gmail.com>
2024-04-21 06:41:41 -0600

    Avances de formularios
    Creacion de formularios

---
+++ ProyectoBD2Grupo5/Forms/FrmProveedorFinca.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBD2Grupo5.Forms
+{
+    public partial class FrmProveedorFinca : Form
+    {
+        public FrmProveedorFinca()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAgregarLote_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAgregarLote_Click_1(object sender, EventArgs e)
+        {
+        }
+    }
+}
