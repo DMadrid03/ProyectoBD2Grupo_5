9c8be4bd74d6fa60dfd8b0e98b9f97195958a93e
Daniel Madrid <danielmadrid@unah.hn>
2024-04-22 23:00:45 -0600

    Agregando procedures para CosechaPago y modificando spCosechaSelect

--- Scripts sql/Cosecha/spCosecha (insert,update,select,Delete).sql
+++ Scripts sql/Cosecha/spCosecha (insert,update,select,Delete).sql
@@ -1,23 +1,28 @@
-
-create or alter procedure spCosechaSelect @CultivoID int = 0, @ProductoID int = 0
+use db20211900096
+go
+create or alter procedure spCosechaSelect @CultivoID int = 0, @ProductoID int = 0,@CosechaID int = 0
 as	
 	select *into #cosecha from Cosecha where (cultivoid = @CultivoID or @cultivoID = 0) and 
-		(ProductoID = @ProductoID or @ProductoID = 0)
+		(ProductoID = @ProductoID or @ProductoID = 0) and(CosechaID = @CosechaID or @CosechaID = 0)
 
 	select *into #Cultivo from cultivo where cultivoid in(select cultivoid from #cosecha)
 	select *into #Lote from lote where loteid in (select loteid from #Cultivo)
 	select *into #Finca from finca where fincaID in (select fincaID from #lote)
 	select *into #Productor from Productor where productorID in(select ProductorID from #Finca)
+	select *into #Unidad from dbo.Unidad()
+	select* into #Producto from Producto
 	
-	select p.Nombre,F.FincaID CodigoFinca, f.Nombre NombreFinca,L.LoteID,
+	select p.Nombre,F.FincaID CodigoFinca, f.Nombre NombreFinca,L.Codigo Lote,Cult.FechaSiembra,Cult.Precio PrecioCultivo,
+	c.CosechaID,c.Cantidad CantidadCosechada,U.Nombre Unidad,c.Fecha FechaCosecha,p.Nombre Producto
 	from #Productor p
 	inner join #Finca F on f.ProductorID = p.ProductorID
 	inner join #Lote L on L.FincaID = f.FincaID
 	inner join #Cultivo cult on cult.LoteID = l.LoteID
 	inner join #cosecha c on c.CultivoID = cult.CultivoID
+	inner join #Unidad u on u.UnidadID = c.UnidadID
+	inner join #Producto pr on pr.ProductoID = c.ProductoID
 go
 ------------------------------------------------------------------------
-
 
 create or alter procedure spCosechaInsert @CosechaID int,@ProductoID int, @CultivoID int, 
 	@UnidadID int, @Cantidad int, @Fecha datetime
